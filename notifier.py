ced7a1ca0bb9c788b42bd16434cc285b2bdef4a8
Bryan Garcia <bryan.garcia.cuevas@gmail.com>
2025-07-20 21:24:49 -0700

    make notifyer for pushover

--- notifier.py
+++ notifier.py
@@ -5,79 +5,51 @@ import sys
 import time
 from hashlib import md5
 from os import isatty
-
-import twitter
+import requests
 
 from enums.emoji import Emoji
 
 MAX_TWEET_LENGTH = 279
 DELAY_FILE_TEMPLATE = "next_{}.txt"
 DELAY_TIME = 1800
-CREDENTIALS_FILE = "twitter_credentials.json"
 
+def send_notification(notif):
+    print(notif)
 
-def _create_tweet(tweet, tc):
-    tweet = tweet[:MAX_TWEET_LENGTH]
-    api = twitter.Api(
-        consumer_key=tc["consumer_key"],
-        consumer_secret=tc["consumer_secret"],
-        access_token_key=tc["access_token_key"],
-        access_token_secret=tc["access_token_secret"],
-    )
-    resp = api.PostUpdate(tweet)
-    # api.CreateFavorite(resp)
-    print("The following was tweeted: ")
-    print()
-    print(tweet)
+    token = 'your_app_token_here'
+    user = 'your_user_key_here'
 
+    data = {
+        'token': token,
+        'user': user,
+        'message': notif
+    }
 
-def main(args, stdin):
-    with open(CREDENTIALS_FILE) as f:
-        tc = json.load(f)
+    response = requests.post('https://api.pushover.net/1/messages.json', data=data)
+    print(f"Status: {response.status_code}, Response: {response.text}")
 
-    # Janky simple argument parsing.
-    if len(args) != 2:
-        print("Please provide the user you want to tweet at!")
-        sys.exit(1)
+    print('notification has been sent!')
 
-    user = args[1].replace("@", "")
-
+def main(stdin):
     first_line = next(stdin)
-    first_line_hash = md5(first_line.encode("utf-8")).hexdigest()
-
-    delay_file = DELAY_FILE_TEMPLATE.format(first_line_hash)
-    try:
-        with open(delay_file, "r") as f:
-            call_time = int(f.read().rstrip())
-    except:
-        call_time = 0
-
-    if call_time + random.randint(DELAY_TIME - 30, DELAY_TIME + 30) > int(
-        time.time()
-    ):
-        print("It is too soon to tweet again")
-        sys.exit(0)
 
     if "Something went wrong" in first_line:
-        _create_tweet("{}, I'm broken! Please help :'(".format(user), tc)
+        send_notification("I'm broken! Please help :'(")
         sys.exit()
 
     available_site_strings = generate_availability_strings(stdin)
 
     if available_site_strings:
-        tweet = generate_tweet_str(available_site_strings, first_line, user)
-        _create_tweet(tweet, tc)
-        with open(delay_file, "w") as f:
-            f.write(str(int(time.time())))
+        tweet = generate_notification_str(available_site_strings, first_line)
+        send_notification(tweet)
         sys.exit(0)
     else:
         print("No campsites available, not tweeting 😞")
         sys.exit(1)
 
 
-def generate_tweet_str(available_site_strings, first_line, user):
-    tweet = "@{}!!! ".format(user)
-    tweet += first_line.rstrip()
+def generate_notification_str(available_site_strings, first_line):
+    tweet = first_line.rstrip()
     tweet += " 🏕🏕🏕\n"
     tweet += "\n".join(available_site_strings)
     tweet += "\n" + "🏕" * random.randint(5, 20)  # To avoid duplicate tweets.
@@ -99,4 +71,4 @@ def generate_availability_strings(stdin):
 
 
 if __name__ == "__main__":
-    main(sys.argv, sys.stdin)
+    main(sys.stdin)
